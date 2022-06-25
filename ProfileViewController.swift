5fdd008ca61a022b101bad0bd5c2224657df9f99
DjMiks <9669374@gmail.com>
2022-06-25 14:24:10 +0300

    v2 Доработка задачи

--- ProfileViewController.swift
+++ ProfileViewController.swift
@@ -10,12 +10,9 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-           
-           
     }
     
 

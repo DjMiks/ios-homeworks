e4ea22f18421840ee9eec6bb730d0ff25b2ee7bf
DjMiks <9669374@gmail.com>
2022-07-03 18:12:46 +0300

    v3 Доработка задачи

--- ProfileView.swift
+++ ProfileView.swift
@@ -10,27 +10,45 @@ import UIKit
 class ProfileView: UIView {
     
     @IBOutlet weak var Image: UIImageView!
-    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var NameLabel: UILabel!
     @IBOutlet weak var Date: UILabel!
     @IBOutlet weak var City: UILabel!
     @IBOutlet weak var Text: UITextView!
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.setupView()
+        self.superView()
     }
-
-    private func setupView () {
-        let view = self.loadViewFromXib()
+    
+    private func superView() {
+        let view = self.loadViewFromNib()
         self.addSubview(view)
         view.frame = self.bounds
-        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-    }
+        }
+    
 
-    private func loadViewFromXib() -> UIView {
-        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)?.first as? UIView else
-        { return UIView() }
-        return view
-    }
+private func loadViewFromNib() -> UIView {
+    guard let view = Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)?.first as? UIView else
+    { return UIView() }
+    return view
+}
+    
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//        self.setupView()
+//    }
+//
+//    private func setupView () {
+//        let view = self.loadViewFromXib()
+//        self.addSubview(view)
+//        view.frame = self.bounds
+//        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//    }
+//
+//    private func loadViewFromXib() -> UIView {
+//        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)?.first as? UIView else
+//        { return UIView() }
+//        return view
+//    }
 
 }
